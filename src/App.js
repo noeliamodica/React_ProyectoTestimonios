731b7cd6b35bbd2d895014e612946d328c116c44
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-02-24 22:06:30 -0300

    agrego props

--- src/App.js
+++ src/App.js
@@ -9,7 +9,35 @@ function App() {
         <div className='contenedor-principal'>
           <h1> Estos son los testimonios de nuestros alumnos </h1>
 
-          <Testimonio/>
+          <Testimonio
+           nombre='Emma Bostian'
+           pais='Sweden'
+           imagen='emma'
+           cargo='Software Engineer'
+           empresa='Spotify'
+           testimonio= 'Ive always struggled with learning JavaScript. Ive taken many courses but freeCodeCamps course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer'
+          />
+
+
+          <Testimonio
+           nombre='Shawn Wang'
+           pais='Singapore'
+           imagen='shawn'
+           cargo='Software Engineer'
+           empresa='Amazon'
+           testimonio= 'Its scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life.Ive always struggled with learning JavaScript. Ive taken many courses but freeCodeCamps course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer'
+          />
+
+
+          <Testimonio
+           nombre='Sarah Chima'
+           pais='Nigeria'
+           imagen='sarah'
+           cargo='Software Engineer'
+           empresa='ChatDesk'
+           testimonio= 'freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.'
+          />
+
         </div>
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -9,14 +9,15 @@ function Testimonio (props){
     <div className='contenedor-testimonio'>
       <img 
         className='imagen-testimonio' 
-        src={require ('../images/testimonio-emma.png')}
+        src={require (`../images/testimonio-${props.imagen}.png`)}
         alt= 'Foto de Emma'
       /> 
 
     <div className='contenedor-texto-testimonio'>
-      <p className='nombre-testimonio'> {props.nombre} en {props.pais} </p>
-      <p className='cargo-testimonio'> {props.cargo}  en {props.empresa} </p>
-      <p className='texto-testimonio'> {props.testimonio} </p>
+      <p className='nombre-testimonio'> 
+      <strong>{props.nombre}</strong> in {props.pais} </p>
+      <p className='cargo-testimonio'> {props.cargo}  in {props.empresa} </p>
+      <p className='texto-testimonio'> "{props.testimonio}" </p>
     </div>
 
     </div>
