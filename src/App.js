7f0407e17202015d65d641cd0777acedfcfe5ca7
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-02-24 21:35:24 -0300

    agrego css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Testimonio from './componentes/Testimonio';
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></link>
+
 
 function App() {
   return (
     <div className="App">
-      <Testimonio/>
+        <div className='contenedor-principal'>
+          <h1> Estos son los testimonios de nuestros alumnos </h1>
+
+          <Testimonio/>
+        </div>
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import '../Stylesheets/Testimonio.css'
 
-function Testimonio (){
+function Testimonio (props){
   
   
   return (
@@ -13,9 +14,9 @@ function Testimonio (){
       /> 
 
     <div className='contenedor-texto-testimonio'>
-      <p className='nombre-testimonio'> Emma Bostian in Sweden </p>
-      <p className='cargo-testimonio'> Software Engineer at Spotify </p>
-      <p className='texto-testimonio'> "I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify." </p>
+      <p className='nombre-testimonio'> {props.nombre} en {props.pais} </p>
+      <p className='cargo-testimonio'> {props.cargo}  en {props.empresa} </p>
+      <p className='texto-testimonio'> {props.testimonio} </p>
     </div>
 
     </div>
